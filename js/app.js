573d29a049f3fce398b2053a05f8533a765e8956
masinwu <emwu83@gmail.com>
2018-12-01 12:41:33 +0000

    Added some basic functionality that stores data in local storage.

--- js/app.js
+++ js/app.js
@@ -32,21 +32,57 @@ function addTweetsToTheListIfTheyExist(storedTweetArray){
         generalListOfTweets.innerHTML = ``;
         for (let i = 0; i < storedTweetArray.length; i++) {
             const element = storedTweetArray[i];
+            /**
+             * Creating new li element, adding css classes
+             */
             newTweetElement = document.createElement("li");
             newTweetElement.classList.add("list-group-item","data");
+            /**
+             * Adding property that will help in identyfying array position of the element
+             */
+            newTweetElement.setAttribute("index-position",i);
             newTweetElement.appendChild(document.createTextNode(`${element}`));
+            /**
+             * Adding font awesome trash icon
+             */
+            let deleteIcon = document.createElement("i");
+            deleteIcon.classList.add("far","fa-trash-alt","float-right", "tweet-delete-icon");
+            /**
+             * Adding the trash can icon to the li element and adding li elelemnt to the ul
+             */
+            newTweetElement.appendChild(deleteIcon);
             generalListOfTweets.appendChild(newTweetElement);
         }
         
     } else {
+        /**
+         * Code that runs if there are no tweets in the array
+         */
         newTweetElement = document.createElement("li");
         newTweetElement.classList.add("list-group-item","text-light", "bg-dark");
+        
         newTweetElement.appendChild(document.createTextNode(`No tweets`));
         generalListOfTweets.appendChild(newTweetElement);
     }
 }
 
 addTweetsToTheListIfTheyExist(storedTweets);
+
+/**
+ * Function that adds element to the array
+ */
+function addElementToTheTweetsArray(tweet,targetArray) {
+    if(tweet != null || tweet != undefined) {
+        targetArray.push(tweet);
+    }
+};
+
+/**
+ * Function that takes index and target array and removes the elelement from that array
+ */
+function removeTweetFromTheArray(tweetIndexPosition,targetArray) {
+    targetArray.splice(tweetIndexPosition,1);
+}
 
 
 /**
@@ -63,4 +99,40 @@ tweetForm.addEventListener("submit", function(e){
         localStorage.setItem("tweets", JSON.stringify(storedTweets));
         addTweetsToTheListIfTheyExist(storedTweets);
     }
+});
+
+
+
+/**
+ * Functionality that removes the elements from the array and reinserts elements to the local storage
+ */
+generalListOfTweets.addEventListener("click",function(e){
+    let clickTarget = e.target;
+    /**
+     * Checking if the click target is the trash can by checking the class of the target item
+     */
+    if (clickTarget.classList.contains("tweet-delete-icon")) {
+        // clickTarget.parentElement.remove();
+        // console.log(clickTarget.parentElement.getAttribute("index-position"));
+        // console.log(storedTweets);
+        /**
+         * Removing the element from the array based on their index
+         */
+        removeTweetFromTheArray(clickTarget.parentElement.getAttribute("index-position"),storedTweets);
+        // console.log(storedTweets);
+        
+
+        /**
+         * Strogin the tweets in the array and writing array to the local storage
+         */
+        addTweetsToTheListIfTheyExist(storedTweets);
+        localStorage.setItem("tweets", JSON.stringify(storedTweets));
+    } else {
+        /**
+         * This is the code that runs if the target element is not a trash can that removes the elements from the list
+         */
+        // console.log("no");
+    
+    }
+    
 });
